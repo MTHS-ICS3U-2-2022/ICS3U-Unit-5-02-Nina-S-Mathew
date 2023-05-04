c60ba4c50cbcc8bdcedbb00e1b191da166293087
Nina-S-Mathew <99273767+Nina-S-Mathew@users.noreply.github.com>
2023-05-04 16:40:45 +0000

    finished python code

--- area.py
+++ area.py
@@ -6,32 +6,34 @@ This is the " value of one number raised to the power of another. " module
 """
 
 
-def calculate_area() -> None:
+def calculate_area(base_f, height_f) -> None:
     """The calculate_area() function calculates area of a rectangle, returns None."""
 
     # process
-    area = base * height * 1/2
+    area = base_f * height_f * 1 / 2
 
     # output
     print(f"The area is {area:.4f} cm²")
     print("")
+
 
 def main() -> None:
     """The main() function just calls other functions, returns None."""
 
     base_str = input("Enter the base of the triangle(cm): ")
     height_str = input("Enter the height of a triangle (cm): ")
-
     try:
-        height_f = float(height_str)
         base_f = float(base_str)
-        if height_f <= 0 or base_f <= 0:
+        height_f = float(height_str)
+        if base_f <= 0 or height_f <= 0:
+            print("Please enter a positive number.")
+        else:
+            calculate_area(base_f, height_f)
     except ValueError:
-        print("Please enter a positive number.")
-
-        calculate_area(height_f, base_f)
+        print("Invalid input.")
 
     print("\nDone.")
+
 
 if __name__ == "__main__":
     main()
